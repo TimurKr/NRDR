153d1fc6cdbf871c57a59dd42d06b35d7b0b1188
Timur Kramár <timurjan.kramar123@gmail.com>
2023-03-27 11:13:16 +0200

    Starting week 7

---
+++ w_7/headers.h
@@ -0,0 +1,10 @@
+//
+// Created by Timur Kramar on 27/03/2023.
+//
+
+#ifndef W_7_HEADERS_H
+#define W_7_HEADERS_H
+
+#endif //W_7_HEADERS_H
+
+#include <stdio.h>
---
+++ w_7/uloha1.c
@@ -0,0 +1,6 @@
+#include "headers.h"
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
