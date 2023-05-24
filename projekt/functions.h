986194c8758520f39cf0b97e261beaf5d824ee52
Timur Kramár <timurjan.kramar123@gmail.com>
2023-05-24 15:53:56 +0200

    initialize project

---
+++ projekt/functions.h
@@ -0,0 +1,18 @@
+//
+// Created by Timur Kramar on 27/03/2023.
+//
+
+#ifndef W_7_FUNCTIONS_H
+#define W_7_FUNCTIONS_H
+
+#endif //W_7_FUNCTIONS_H
+
+#include "math.h"
+
+double x(double x, double y) {
+    return - 1./2 * y;
+}
+
+double y(double x, double y) {
+    return - x - y;
+}
